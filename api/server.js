4454ccad09c8611e8cfba99f482969379ecac363
Bamidele Isaac <bamix95@gmail.com>
2025-09-04 07:42:53 -0700

    connected to database

--- api/server.js
+++ api/server.js
@@ -1,7 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server running on port: 5000");
-});
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Failed to start server", error);
+
+    process.exit(1);
+  }
+};
+startServer();
