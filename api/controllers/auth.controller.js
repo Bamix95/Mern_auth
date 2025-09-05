304769fcf1a64b27d98d2205a2c3dd7f68900fd1
Bamidele Isaac <bamix95@gmail.com>
2025-09-05 05:43:11 -0700

    Created sign up api

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  try {
+    const inValidUser = await User.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (inValidUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Sign Up Successful",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/server.js
+++ api/server.js
@@ -1,12 +1,35 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+//Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error("Error:", err);
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
 
 const startServer = async () => {
   try {
